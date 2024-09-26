8ebbd922f91e2a193264bab5bdeebf1aa6d9576b
jsh9057 <jsh9057@skuniv.ac.kr>
2024-09-26 19:57:53 +0900

    ErrorType 수정및 추가

--- src/main/java/dev/be/oneday/exception/ErrorType.java
+++ src/main/java/dev/be/oneday/exception/ErrorType.java
@@ -9,10 +9,14 @@ import org.springframework.http.HttpStatus;
 public enum ErrorType {
 
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "COMMON-001","입력받은 인자값이 잘못되었습니다."),
-    VALUE_IS_NULL(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON-002","Value is null"),
+    VALUE_IS_NULL(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON-002","값이 비어있습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"COMMON-003", "예상치 못한 서버 에러입니다."),
 
     HABIT_NOT_FOUND(HttpStatus.NOT_FOUND,"HABIT-001","습관을 찾을 수 없습니다."),
-    ALREADY_JOINED_HABIT(HttpStatus.BAD_REQUEST, "HABIT-002", "이미 가입된 습관입니다."),
+    ALREADY_JOINED_HABIT(HttpStatus.BAD_REQUEST, "HABIT-002", "이미 참여한 습관입니다."),
+    NOT_JOINED_HABIT(HttpStatus.BAD_REQUEST, "HABIT-003", "참여하지않은 습관입니다."),
+    ALREADY_CHECKED_HABIT(HttpStatus.BAD_REQUEST,"HABIT-004", "이미 실천 또는 실천하지 못한 습관입니다."),
+    HABIT_CHECK_NOT_FOUND(HttpStatus.NOT_FOUND, "HABIT-005","존재하지않는 습관 실천여부 입니다."),
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"USER-001","유저를 찾을 수 없습니다.");
 
