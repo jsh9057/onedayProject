5622c24f33ba122d12483345dcb6ff51ca88d39e
jsh9057 <jsh9057@skuniv.ac.kr>
2024-10-04 19:19:21 +0900

    UserAccount의 unique 필드 누락 수정

--- src/main/java/dev/be/oneday/domain/UserAccount.java
+++ src/main/java/dev/be/oneday/domain/UserAccount.java
@@ -21,11 +21,11 @@ public class UserAccount extends BaseEntity{
     @Column(nullable = false, updatable = false)
     private Long userAccountId;
 
-    @Column(nullable = false, columnDefinition = "varchar(50) COMMENT '로그인 아이디'")
+    @Column(unique = true, nullable = false, columnDefinition = "varchar(50) COMMENT '로그인 아이디'")
     private String userId;
     @Column(nullable = false, columnDefinition = "varchar(255) COMMENT '로그인 비밀번호'")
     private String password;
-    @Column(nullable = false, columnDefinition = "varchar(100) COMMENT '다른 사용자에게 노출될 이름'")
+    @Column(unique = true, nullable = false, columnDefinition = "varchar(100) COMMENT '다른 사용자에게 노출될 이름'")
     private String nickname;
     @Column(nullable = false, columnDefinition = "varchar(100) COMMENT '이메일'")
     private String email;
